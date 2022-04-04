c19f3d261d0de0d7b2934bc176e407dccac29d4b
Chang Zhang <chang.zhang0220@icloud.com>
2022-04-04 22:05:59 +0800

    ..

--- main.jl
+++ main.jl
@@ -3,17 +3,3 @@ addprocs(4)
 @everywhere include("sonar.jl")
 NC = SharedArray{Float64}(100)
 E  = SharedArray{Float64}(100)
-@distributed for n = 1:100
-    println("Running Simulation $(n)")
-<<<<<<< HEAD
-<<<<<<< HEAD
-    R = SMC(10000,100,U0,U,61,0.5,0.2,initDist,1)
-=======
-    R = SMC(10000,100,U0,U,61,0.5,0.5,initDist)
->>>>>>> parent of 6e48659 (....)
-=======
-    R = SMC(10000,100,U0,U,61,0.5,0.5,initDist)
->>>>>>> parent of 6e48659 (....)
-    NC[n] = sum(log.(mean(exp.(R.logW[:,2:end]),dims=1)))
-    E[n]  = sum(R.W[:,end].*mean(R.X[end],dims=2))
-end
