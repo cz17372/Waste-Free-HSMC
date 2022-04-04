d6f80cbe4205e7dec75597b8395c3651afa9b3fc
Chang Zhang <chang.zhang0220@icloud.com>
2022-04-04 22:04:57 +0800

    ..

--- main.jl
+++ main.jl
@@ -6,7 +6,11 @@ E  = SharedArray{Float64}(100)
 @distributed for n = 1:100
     println("Running Simulation $(n)")
 <<<<<<< HEAD
+<<<<<<< HEAD
     R = SMC(10000,100,U0,U,61,0.5,0.2,initDist,1)
+=======
+    R = SMC(10000,100,U0,U,61,0.5,0.5,initDist)
+>>>>>>> parent of 6e48659 (....)
 =======
     R = SMC(10000,100,U0,U,61,0.5,0.5,initDist)
 >>>>>>> parent of 6e48659 (....)
