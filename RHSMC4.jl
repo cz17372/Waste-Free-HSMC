d9f7d18ae86dea53565ffe42fd29752efd85ac0a
Chang Zhang <chang.zhang0220@icloud.com>
2022-05-05 18:31:40 +0800

    ....

---
+++ RHSMC4.jl
@@ -0,0 +1,47 @@
+using Distributed, SharedArrays,DataFrames,CSV
+function run_exp(N,M,ϵ,α,λ,model,method;mass_mat,silence=true)
+    Time = SharedArray{Float64}(100)
+    NC   = SharedArray{Float64}(100)
+    MM  = SharedArray{Float64}(100)
+    @sync @distributed for n = 1:100
+        if !silence
+            println("Running Simulation for index $(n)")
+        end
+        t = @timed R = WasteFree.SMC(N,M,model=model,λ=λ,ϵ=ϵ,α=α,method=method,mass_mat=mass_mat);
+        Time[n] = t.time - t.gctime
+        NC[n] = sum(log.(mean(exp.(R.logW),dims=1)))
+        MM[n] = sum(R.W[:,end].*mean(R.X[end],dims=2))
+    end
+    filename = "data/"*model.name*"/"*method*"4.csv"
+    df = CSV.read(filename,DataFrame)
+    dat_name = "N$(N)M$(M)"
+    df[!,dat_name*"_Time"] = Time
+    df[!,dat_name*"_NC"] = NC
+    df[!,dat_name*"_MM"] = MM
+    CSV.write(filename,df)
+    rmprocs(procs()[2:end])
+    return nothing
+end
+println("Enter the number of workers")
+nprocs = readline()
+nprocs = parse(Int64,nprocs)
+println("Enter N")
+N = readline()
+N = parse(Int64,N)
+N = [N];
+M = [50,150,200,250,300,400,500,600];
+df = DataFrame("exprid" => collect(1:100))
+CSV.write("data/sonar/full4.csv",df)
+for n in N
+    for m in M
+        addprocs(nprocs)
+        @everywhere include("Models/sonar.jl")
+        @everywhere include("src/WasteFree3.jl")
+        @everywhere using Distributed, DistributedArrays,JLD2
+        @everywhere using Statistics, StatsBase
+        @everywhere @load "data.jld2" λ τ
+        println("Running experiments for N = $(n), M = $(m), mass_mat = identity,method=full")
+        run_exp(n,m,τ/div(n,m),0.5,λ,sonar,"full",mass_mat="identity",silence=false)
+        rmprocs(procs()[2:end])
+    end
+end
--- adaptation.jl
+++ adaptation.jl
@@ -18,16 +18,14 @@ function ESJD(R,M,P)
     end
     return out
 end
-function SingleJumpVariance(X,H,P,M)
-    W = zeros(M,P)
+function SingleJumpVariance(X,W,P,M)
     D = zeros(M,P)
     JumpVar = zeros(M,P)
     for n = 1:M
-        W[n,:] = exp.(H[(n-1)*P+1:n*P] .- H[(n-1)*P+1])
         for i = 1:P
             D[n,i] = norm(X[(n-1)*P+i,:] .- X[(n-1)*P+1,:])^2
         end
-        JumpVar[n,:] = cumsum(W[n,:] .* D[n,:]) ./ (cumsum(W[n,:]))
+        JumpVar[n,:] = cumsum(W[(n-1)*P+1:n*P] .* D[n,:]) ./ cumsum(W[(n-1)*P+1:n*P])
     end
     return mapslices(mean,JumpVar,dims=1)[1,:]
 end
@@ -35,7 +33,7 @@ function JumpVariance(R,M,P)
     T = length(R.λ)-1
     out = zeros(T,P)
     for n = 1:T
-        out[n,:] = SingleJumpVariance(R.X[n+1],R.H[:,n+1],P,M)
+        out[n,:] = SingleJumpVariance(R.X[n+1],R.W[:,n+1],P,M)
     end
     return out
 end
@@ -60,16 +58,54 @@ function LogNC(R)
     return sum(log.(mean(exp.(R.logW),dims=1)[1,:]))
 end
 getmaxidx(x) = findmax(x)[2]
+function SingleTrajESS(W,M,P)
+    res = zeros(M,P)
+    for n = 1:M
+        tempW = W[(n-1)*P+1:n*P]
+        for t = 1:P
+            res[n,t] = (1/sum((tempW[1:t] / sum(tempW[1:t])).^2))/t
+        end
+    end
+    return mapslices(mean,res,dims=1)[1,:]
+end
+function TrajESS(R,M,P)
+    T = length(R.λ) -1
+    output = zeros(T,P)
+    for n = 1:T
+        output[n,:] = SingleTrajESS(R.W[:,n+1],M,P)
+    end
+    return output
+end
+function plotTrajESS(R,M,P;col="Blues",fig=nothing)
+    T = length(R.λ) -1
+    c = colormap(col,T+10);
+    out = TrajESS(R,M,P)
+    if isnothing(fig)
+        plot(out[1,:],xlabel="t",ylabel="%ESS",label="",color=c[1])
+        for n=2:T
+            plot!(out[n,:],label="",color=c[n])
+        end
+        current()
+    else
+        for n=1:T
+            plot!(fig,out[n,:],label="",color=c[n])
+        end
+        current()
+    end
+end
 
-N = 40000; M = 100; P = div(N,M)
+
+N = 10000; M = 100; P = div(N,M)
 include("src/WasteFree4.jl")
-R = WasteFree.SMC(N,M,model=sonar,ϵ=0.1,α=0.5,method="full",mass_mat="identity",printl=true);
+R = WasteFree.SMC(N,M,model=sonar,ϵ=0.05,α=0.5,method="full",mass_mat="identity",printl=true);
 fig1 = plotJumpVar(R,0.1,M,P)
+fig2 = plotTrajESS(R,M,P)
+plotTrajESS(R,M,P,col="Purples",fig=fig2)
 JV = JumpVariance(R,M,P)
 plot(mapslices(getmaxidx,JV,dims=2)[:,1] * 0.1)
 
 N = 30000; M = 100; P = div(N,M)
-R = WasteFree.SMC(N,M,model=sonar,ϵ=20/P,α=0.5,method="full",mass_mat="identity",printl=true);
+R = WasteFree.SMC(N,M,model=sonar,ϵ=20/P,α=0.2,method="full",mass_mat="identity",printl=true);
 LogNC(R)
 
 λ = R.λ
@@ -113,3 +149,6 @@ bp11 = box(y=data.N10000M200eps15alpha50_NC,name="M=200,fix_eps=0.15")
 bp12 = box(y=data.N10000M200eps20alpha50_NC,name="M=200,fix_eps=0.2")
 
 M20 = CSV.read("M20.csv",DataFrame)
+
+
+    
